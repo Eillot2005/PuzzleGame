7dc25eed24f7551a47bc96accb4f411946c80d90
lyx <doo8opnewman@outlook.com>
2024-02-12 21:42:09 +0800

    PuzzleGame.iml

---
+++ puzzleGame/src/ui/loginJFrame.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.*;
+
+public class loginJFrame extends JFrame {
+    public loginJFrame()
+    {
+        setSize(488,430);
+        this.setTitle("登录");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        setDefaultCloseOperation(3);
+        setVisible(true);
+    }
+}
